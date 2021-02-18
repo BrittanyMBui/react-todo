c9dbd81dfa84514e960600dd2100f8f55be86faf
brittanymbui <brittymbui@gmail.com>
2021-02-18 11:51:26 -0800

    Adds a form to create and add a todo

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TodoModel from '../models/Todo';
 import TodoList from '../components/TodoList';
+import CreateTodoForm from '../components/CreateTodoForm';
 
 
 class TodosContainer extends React.Component {
@@ -20,10 +21,29 @@ class TodosContainer extends React.Component {
         });
     };
 
+    createTodo = (todo) => {
+        let newTodo = {
+            body: todo,
+            completed: false,
+        };
+
+        TodoModel.create(newTodo)
+        .then((res) => {
+            let todos = this.state.todos;
+            todos.push(res.data);
+            this.setState({
+                todos: todos
+            });
+        });
+    };
+
     render() {
 
         return(
             <div className="todosComponent">
+                <CreateTodoForm
+                    createTodo={this.createTodo}
+                />
                 <TodoList
                     todos={this.state.todos}
                 />
